53ce4ac45479482b16a4d74339942267a01f30cc
Mehul Joshi <mehul0923@gmail.com>
2018-04-30 17:39:07 +0530

    Added demo page to project

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -5,20 +5,32 @@ module.exports = {
     entry: [
         path.join(__dirname, '../src/index.js')
     ],
+    mode: 'development',
     module: {
-        loaders: [{
+        rules:[{
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             loader: 'babel-loader'
         },{
             test: /\.scss$/,
             loaders: ["style-loader", "css-loader", "sass-loader"]
-        }
-        ]
+        }]
     },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     devServer: {
-        contentBase: __dirname + '/dist',
-        historyApiFallback: true
+        contentBase: path.join(__dirname,  '../dist'),
+        historyApiFallback: true,
+        hot: true
+    },
+    output: {
+        path: path.join(__dirname,  '../dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
     },
 
 };
